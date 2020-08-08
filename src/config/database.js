7f9050a9424524c73cd7508f9d0581aecd0690b7
Vanessa Maria <vanessamfc@outlook.com>
2020-08-08 10:13:37 -0300

    feat/add prod config

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  username: "vanessa",
-  password: "docker",
-  database: "my_movie_list",
-  host: "127.0.0.1",
-  dialect: "postgres",
+  username: process.env.DB_USER || 'vanessa',
+  password: process.env.DB_PASSWORD || 'docker',
+  database: process.env.DB_DATABASE || 'my_movie_list',
+  host: process.env.DB_HOST || '127.0.0.1',
+  dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   define: {
     timestamps: true,
